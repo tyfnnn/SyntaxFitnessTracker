1889ffd26b61f2bd03f889e0a3e487af040a0a21
tyfnnn <tayfun.i@icloud.com>
2025-06-03 23:20:43 +0200

    DataConverter.kt

--- app/src/main/java/com/example/syntaxfitness/data/local/database/FitnessDatabase.kt
+++ app/src/main/java/com/example/syntaxfitness/data/local/database/FitnessDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.syntaxfitness.data.local.converter.DateConverters
 import com.example.syntaxfitness.data.local.dao.RunDao
 import com.example.syntaxfitness.data.local.entity.RunEntity
 
@@ -12,7 +14,7 @@ import com.example.syntaxfitness.data.local.entity.RunEntity
     version = 1,
     exportSchema = false
 )
-
+@TypeConverters(DateConverters::class)
 abstract class FitnessDatabase : RoomDatabase() {
     abstract fun runDao(): RunDao
 
